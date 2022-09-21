a074456fc4b5e6efe61e0cd608e8159433b2faa8
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2022-09-21 16:23:10 -0300

    Arquivos da aula 01

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+var nome = 'Maria de Fátima'
+var notaPrimeiroBimestre = 9
+var notaSegundoBimestre = 8
+var notaTerceiroBimestre = 8
+var notaQuartoBimestre = 10
+
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(0)
+
+console.log("Bem-vindo(a), " + nome)
+console.log(notaFixada)
+
